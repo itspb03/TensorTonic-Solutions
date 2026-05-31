f3e3bae9cdbcd971997164dc672c3fd3f08b1214
Pranay Bhaskar <pranay.bhaskar03112@gmail.com>
2026-05-31 17:08:14 +0530

    Add solution for sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    x=np.asarray(x)
+    return np.where(
+        x >= 0,
+        1 / (1 + np.exp(-x)),
+        np.exp(x) / (1 + np.exp(x))
+    )
+    # Write code here
+    
